fa3e4db2ddd666133c78cc0fddcdd80f0c4a7201
ZachernGithub <143934313+ZachernGithub@users.noreply.github.com>
2023-09-03 18:05:25 +0800

    1.0.2-alpha
    -added loops
    -added a error message where you can't divide zero
    -removed some bugs/errors

--- main.py
+++ main.py
@@ -1,33 +1,90 @@
-operator = input("Choose Your Operator (+, -, *, /) : ")
+operator = ""
 
-if operator == '+' :
-    first_number = int(input("What Is Your First Number? : "))
-    second_number = int(input("What Is Your Second Number? : "))
+while len(operator) == 0:
+    operator = input("Choose Your Operator (+, -, *, /) : ")
+
+if operator == '+':
+    first_number = ""
+    second_number = ""
+    
+    while first_number == "":
+        try:
+            first_number = int(input("What Is Your First Number? : "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
+    while second_number == "":
+        try:
+            second_number = int(input("What Is Your Second Number? : "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
     answer = first_number + second_number
     
     print("First Number + Second Number = " + str(answer))
     
-elif operator == '-' :
-    first_number = int(input("What Is Your First Number? : "))
-    second_number = int(input("What Is Your Second Number? : "))
+elif operator == '-':
+    first_number = ""
+    second_number = ""
+    
+    while first_number == "":
+        try:
+            first_number = int(input("What Is Your First Number? : "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
+    while second_number == "":
+        try:
+            second_number = int(input("What Is Your Second Number? : "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
     answer = first_number - second_number
     
     print("First Number - Second Number = " + str(answer))
+   
+elif operator == '*':
+    first_number = ""
+    second_number = ""
     
-elif operator == '*' :
-    first_number = int(input("What Is Your First Number? : "))
-    second_number = int(input("What Is Your Second Number? : "))
+    while first_number == "":
+        try:
+            first_number = int(input("What Is Your First Number? : "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
+    while second_number == "":
+        try:
+            second_number = int(input("What Is Your Second Number? : "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
     answer = first_number * second_number
     
-    print("First Number × Second Number = " + str(answer))
+    print("First Number * Second Number = " + str(answer))
     
 elif operator == '/':
-    first_number = int(input("What Is Your First Number? : "))
-    second_number = int(input("What Is Your Second Number? : "))
-    answer = first_number / second_number
+    first_number = ""
+    second_number = ""
     
-    print("First Number ÷ Second Number = " + str(answer))
+    while first_number == "":
+        try:
+            first_number = int(input("What Is Your First Number? : "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
+    while second_number == "":
+        try:
+            second_number = int(input("What Is Your Second Number? : "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    
+    if second_number == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        answer = first_number / second_number
+        print("First Number / Second Number = " + str(answer))
     
 else:
     print("That Was An Invalid Operator.")
-    print("Please Use This Operators (+, -, *, /)")
+    print("Please Use These Operators (+, -, *, /)")
