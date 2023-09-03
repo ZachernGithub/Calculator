08745432dda8eb6e5f0f7c92d738bc7eae609912
ZachernGithub <143934313+ZachernGithub@users.noreply.github.com>
2023-09-03 17:42:39 +0800

    Calculator 1.0
    Structure for this project

---
+++ main.py
@@ -0,0 +1,8 @@
+first_number = int(input("What Is Your First Number? : "))
+second_number = int(input("What Is Your Second Number? : "))
+
+answer = first_number + second_number
+
+print("Your First Number Is " + str(first_number))
+print("Your Second Number Is "  + str(second_number))
+print("First Number + Second Number = " + str(answer))
